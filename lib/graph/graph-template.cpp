2a0ef592e4814266a47eb635a1c70a997e6ded7d
Tqk <shktmygs@gmail.com>
2020-03-03 03:35:38 +0900

    [+] graph template

---
+++ lib/graph/graph-template.cpp
@@ -0,0 +1,13 @@
+#define IGNORE
+using length=lint;
+struct edge{
+	int src,to;
+	length cost;
+
+	edge(int src,int to,length cost): src(src),to(to),cost(cost){}
+};
+
+using Edges=vector<edge>;
+using Weighted=vector<Edges>;
+using UnWeighted=vector<vector<int> >;
+using Matrix=vector<vector<length> >;
