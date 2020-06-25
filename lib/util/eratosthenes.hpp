10a6582245a522ea463b3c59df0a2566160cb74f
tqkoh <shktmygs@gmail.com>
2020-06-25 18:07:52 +0900

    [*] utf-8

--- lib/util/eratosthenes.hpp
+++ lib/util/eratosthenes.hpp
@@ -1,6 +1,6 @@
 #pragma once
-int cn[MAXN];// ‡¬”‚È‚ç1, ‘f”‚È‚ç0
-void erat(lint n){// O(nlogn)
+int cn[MAXN]; // prime:0
+void erat(lint n){
 	for(int i=2; i<=n ++i){
 		if(!cn[i]){
 			for(int j=2*i; j<=n; j+=i)cn[j]=1;
