2dacf0be03dfa192ab299bbce0d244eaff6896de
tqkoh <shktmygs@gmail.com>
2020-06-25 16:47:14 +0900

    [+] eratosthenes

---
+++ lib/util/eratosthenes.hpp
@@ -0,0 +1,14 @@
+#pragma once
+int cn[MAXN];//‡¬”‚È‚ç1, ‘f”‚È‚ç0
+void erat(lint n){//O(nlogn)
+	for(int i=2; i<=n ++i){
+		if(!cn[i]){
+			for(int j=2*i; j<=n; j+=i)cn[j]=1;
+			//i is prime
+		}
+	}
+}
+
+/*
+* @title eratosthenes
+*/
