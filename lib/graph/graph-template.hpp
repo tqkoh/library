87599b3ff0118082e92628683e107b775e27a1fb
tqkoh <shktmygs@gmail.com>
2021-11-09 11:33:36 +0900

    change graph structure

--- lib/graph/graph-template.hpp
+++ lib/graph/graph-template.hpp
@@ -1,37 +1,38 @@
 #pragma once
-using length=lint;
+using length = lint;
 struct edge{
-	int src,to;
-	length cost;
-	edge()=default;
-	edge(int src,int to,length cost): src(src),to(to),cost(cost){}
+	int src, to, id;
+	edge() = default;
+	edge(int src, int to, int id = 0): src(src), to(to), id(id){}
 };
 
-using Edges=vector<edge>;
-using Weighted=vector<Edges>;
-using UnWeighted=vector<vector<int> >;
-using Matrix=vector<vector<length> >;
+using Edges = vector<edge>;
+class Weighted: public vector<Edges>{
+public:
+	Weighted(int n){
+		this->resize(n);
+	}
+	void add_edge(edge e, bool directed = 0){
+		(*this)[e.src].push_back(e);
+		if(!directed)(*this)[e.to].push_back(edge(e.to, e.src, e.id));
+	}
+	void add_edge(int src, int to, int id = 0, bool directed = 0){
+		(*this)[src].push_back(edge(src, to, id));
+		if(!directed)(*this)[to].push_back(edge(to, src, id));
+	}
+};
+class Matrix: public vector<vector<length>>{
+public:
+	Matrix(int n, length inf = 2*linf){
+		this->resize(n, vector<length>(n, inf));
+		rep(n)(*this)[i][i] = 0;
+	}
+};
 
-void e2w(Weighted& g,Edges edges,bool directed){
-	for(auto& e:edges){
-		g[e.src].push_back(e);
-		if(!directed)g[e.to].push_back(edge(e.to,e.src,e.cost));
-	}
-}
-void e2u(UnWeighted& g,Edges edges,bool directed){
-	for(auto& e:edges){
-		g[e.src].push_back(e.to);
-		if(!directed)g[e.to].push_back(e.src);
-	}
-}
-void e2m(Matrix& g,Edges edges,bool directed,length infinity){
-	for(auto& e:g)e.resize(g.size(),infinity);
-	for(int i=0;i<g.size();++i)g[i][i]=0;
-	for(auto& e:edges){
-		g[e.src][e.to]=min(g[e.src][e.to],e.cost);
-		if(!directed)g[e.to][e.src]=min(g[e.to][e.src],e.cost);
-	}
-}
+
+/*
+* @title Graph Template
+*/
 
 
 /*
--- lib/graph/warshall-floyd.hpp
+++ lib/graph/warshall-floyd.hpp
@@ -1,14 +1,14 @@
 #pragma once
-void warshall_floyd(Matrix &g,length inf){
-	for(int k=0;k<g.size();++k){
-		for(int i=0;i<g.size();++i){
-			for(int j=0;j<g.size();++j){
-				if(g[i][k]==inf||g[k][j]==inf)continue;
-				g[i][j]=min(g[i][j],g[i][k]+g[k][j]);
-			}
-		}
-	}
-}
+// void warshall_floyd(Matrix &g,length inf){
+// 	for(int k=0;k<g.size();++k){
+// 		for(int i=0;i<g.size();++i){
+// 			for(int j=0;j<g.size();++j){
+// 				if(g[i][k]==inf||g[k][j]==inf)continue;
+// 				g[i][j]=min(g[i][j],g[i][k]+g[k][j]);
+// 			}
+// 		}
+// 	}
+// }
 /*
 * @title Warshall Floyd
 */
