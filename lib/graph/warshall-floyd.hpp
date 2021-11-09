ac93eecfa4b846a0994fc3f29272ce54d6d4b158
tqkoh <shktmygs@gmail.com>
2021-11-09 16:00:33 +0900

    fix warshall floyd

--- lib/graph/warshall-floyd.hpp
+++ lib/graph/warshall-floyd.hpp
@@ -1,14 +1,19 @@
 #pragma once
-// void warshall_floyd(Matrix &g,length inf){
-// 	for(int k=0;k<g.size();++k){
-// 		for(int i=0;i<g.size();++i){
-// 			for(int j=0;j<g.size();++j){
-// 				if(g[i][k]==inf||g[k][j]==inf)continue;
-// 				g[i][j]=min(g[i][j],g[i][k]+g[k][j]);
-// 			}
-// 		}
-// 	}
-// }
+int warshall_floyd(Matrix& g, length inf = 2*linf){
+	int n = g.size();
+	for(int k = 0; k<n; ++k){
+		for(int i = 0; i<n; ++i){
+			for(int j = 0; j<n; ++j){
+				if(g[i][k]==inf||g[k][j]==inf)continue;
+				g[i][j] = min(g[i][j], g[i][k]+g[k][j]);
+			}
+		}
+	}
+	for(int i = 0; i<n; ++i){
+		if(g[i][i]<0)return 1;
+	}
+	return 0;
+}
 /*
 * @title Warshall Floyd
 */
