04252d79257e36cd6587fbddcee0b962def0eb17
tqkoh <shktmygs@gmail.com>
2021-11-11 17:13:41 +0900

    add doubling test

--- lib/util/doubling.hpp
+++ lib/util/doubling.hpp
@@ -4,11 +4,11 @@ class Doubling{
 public:
 	vector<vector<lint>>d;
 	lint l = 0;
-	Doubling(vector<lint>a, lint maxpow = -1){
-		if(maxpow<0)maxpow = a.size();
-		while((1LL<<l)<=maxpow)++l;
-		d.resize(l, a);
-		rep(p, l-1)rep(a.size())d[p+1][i] = d[p][d[p][i]];
+	Doubling(vector<lint>f, lint maxpow = -1){
+		if(maxpow<0)maxpow = f.size();
+		l = log2(maxpow)+1;
+		d.resize(l, f);
+		for(int p = 0; p<l-1; ++p)for(int i=0; i<f.size(); ++i)d[p+1][i] = d[p][d[p][i]];
 	}
 	lint operator()(lint pow, lint i){
 		for(lint j = 0; pow; ++j){
